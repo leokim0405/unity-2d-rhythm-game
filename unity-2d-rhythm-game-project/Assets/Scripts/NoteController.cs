883da87bf72475e636b0785d9a289cd7cf04efc6
leokim0405 <48847503+leokim0405@users.noreply.github.com>
2019-11-05 02:04:51 +0900

    비트 제작

--- unity-2d-rhythm-game-project/Assets/Scripts/NoteController.cs
+++ unity-2d-rhythm-game-project/Assets/Scripts/NoteController.cs
@@ -1,5 +1,7 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
 
 public class NoteController : MonoBehaviour
@@ -20,7 +22,7 @@ public class NoteController : MonoBehaviour
     private ObjectPooler noteObjectPooler;
     private List<Note> notes = new List<Note>();
     private float x, z, startY = 8.0f;
-    private float beatInterval = 1.0f;
+    //private float beatInterval = 1.0f;
 
     void MakeNote(Note note)
     {
@@ -32,11 +34,20 @@ public class NoteController : MonoBehaviour
         obj.SetActive(true);
     }
 
+    private string musicTitle;
+    private string musicArtist;
+    private int bpm;
+    private int divider;
+    private float startingPoint;
+    private float beatCount;
+    private float beatInterval;
+
+
     IEnumerator AwaitMakeNote(Note note)
     {
         int noteType = note.noteType;
         int order = note.order;
-        yield return new WaitForSeconds(order * beatInterval);
+        yield return new WaitForSeconds(startingPoint + order * beatInterval);
         MakeNote(note);
     }
     // Start is called before the first frame update
@@ -44,14 +55,28 @@ public class NoteController : MonoBehaviour
     void Start()
     {
         noteObjectPooler = gameObject.GetComponent<ObjectPooler>();
-        notes.Add(new Note(1, 1));
-        notes.Add(new Note(2, 2));
-        notes.Add(new Note(3, 3));
-        notes.Add(new Note(4, 4));
-        notes.Add(new Note(1, 5));
-        notes.Add(new Note(2, 6));
-        notes.Add(new Note(3, 7));
-        notes.Add(new Note(4, 8));
+
+        TextAsset textAsset = Resources.Load<TextAsset>("Beats/" + GameManager.instance.music);
+        StringReader reader = new StringReader(textAsset.text);
+        musicTitle = reader.ReadLine();
+        musicArtist = reader.ReadLine();
+
+        string beatInformation = reader.ReadLine();
+        bpm = Convert.ToInt32(beatInformation.Split(' ')[0]);
+        divider = Convert.ToInt32(beatInformation.Split(' ')[1]);
+        startingPoint = (float)Convert.ToDouble(beatInformation.Split(' ')[2]);
+        beatCount = (float)bpm / divider;
+        beatInterval = 1 / beatCount;
+        string line;
+        while((line = reader.ReadLine()) != null)
+        {
+            Note note = new Note(
+                Convert.ToInt32(line.Split(' ')[0]) + 1,
+                Convert.ToInt32(line.Split(' ')[1])
+                );
+            notes.Add(note);
+        }
+
         for (int i = 0; i < notes.Count; i++)
         {
             StartCoroutine(AwaitMakeNote(notes[i]));
