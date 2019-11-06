d90a217fc51472b9409d842f169abecc525b3fe8
leokim0405 <48847503+leokim0405@users.noreply.github.com>
2019-11-07 00:34:31 +0900

    랭크 보여주기 및 게임 시작화면을 돌아가기

--- unity-2d-rhythm-game-project/Assets/Scripts/GameResultManager.cs
+++ unity-2d-rhythm-game-project/Assets/Scripts/GameResultManager.cs
@@ -2,18 +2,53 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using System.IO;
+using System;
+using UnityEngine.SceneManagement;
 
 public class GameResultManager : MonoBehaviour
 {
     public Text musicTitleUI;
     public Text scoreUI;
     public Text maxComboUI;
+    public Image RankUI;
+
     // Start is called before the first frame update
     void Start()
     {
         musicTitleUI.text = PlayerInformation.musicTitle;
-        scoreUI.text = "" + PlayerInformation.score;
-        maxComboUI.text = "" + PlayerInformation.maxCombo;
+        scoreUI.text = "점수: " + (int)PlayerInformation.score;
+        maxComboUI.text = "최대 콤보: " + PlayerInformation.maxCombo;
+        TextAsset textAsset = Resources.Load<TextAsset>("Beats/" + PlayerInformation.selectMusic);
+        StringReader reader = new StringReader(textAsset.text);
+        reader.ReadLine();
+        reader.ReadLine();
+        string beatInformation = reader.ReadLine();
+        int scoreS = Convert.ToInt32(beatInformation.Split(' ')[3]);
+        int scoreA = Convert.ToInt32(beatInformation.Split(' ')[4]);
+        int scoreB = Convert.ToInt32(beatInformation.Split(' ')[5]);
+
+        if(PlayerInformation.score >= scoreS)
+        {
+            RankUI.sprite = Resources.Load<Sprite>("Sprites/Rank S");
+        }
+        else if(PlayerInformation.score >= scoreA)
+        {
+            RankUI.sprite = Resources.Load<Sprite>("Sprites/Rank A");
+        }
+        else if (PlayerInformation.score >= scoreB)
+        {
+            RankUI.sprite = Resources.Load<Sprite>("Sprites/Rank B");
+        }
+        else
+        {
+            RankUI.sprite = Resources.Load<Sprite>("Sprites/Rank C");
+        }
+    }
+
+    public void Replay()
+    {
+        SceneManager.LoadScene("SongSelectScene");
     }
 
     // Update is called once per frame
